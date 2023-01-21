4736716a0ab669ffdcf10aeb4c16cf67fec7de7e
GoitomHailu <goitomdesta19@gmail.com>
2023-01-20 22:17:41 -0500

    my initial commit

---
+++ CraftCommon_17_Java_Exercise/src/com/icraft/day4/DataTypesDemo.java
@@ -0,0 +1,30 @@
+package com.icraft.day4;
+
+public class DataTypesDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+           
+		byte myNum = 19;
+		short myValue = 10;
+		int noOfRooms = 200;
+		long distanceToSun = 98000000L;
+		
+		double weightofpotato = 5.984;
+		float myFloarNum = 5.99f;
+		
+		char myLetter = 'D';
+		boolean myBool = true;
+		
+		System.out.println("byte: " + myNum);
+		System.out.println("short: " + myValue);
+		System.out.println("int: " + noOfRooms);
+		System.out.println("Long: " + distanceToSun);
+		System.out.println("double: " + weightofpotato);
+		System.out.println("float: " + myFloarNum);
+		System.out.println("char: " + myLetter);
+		System.out.println("boolean: " + myBool);
+		
+	}
+
+}
